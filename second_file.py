2eb3d7a7d1c99896a19e8a73a0a4aaaa7c996f32
rama17aneesh <aneeshtumuluri@gmail.com>
2024-09-04 03:13:00 -0500

    added name parameter

--- second_file.py
+++ second_file.py
@@ -4,8 +4,8 @@ def print_hello_world():
 
      print("Hello world!")
 
-def print_my_name():
-     print("HI! MY NAME IS RAM!")
+def print_my_name(name):
+     print("HI! MY NAME IS {name}!")
 
 print_hello_world()
 print_my_name()
